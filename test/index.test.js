46174371311692a0b7c16bf836c90cb416eb7754
Chris Holden <christopherholden@hotmail.com>
2020-12-29 15:31:47 +0000

    Simplified tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const sut = require('../index');
 const path = require('path');
+
+const sut = require('../index');
 
 sut.setup(path.join(__dirname, 'menu'), path.join(__dirname, './field_defaults'));
 
@@ -13,15 +14,15 @@ test('basic sql preparation', () => {
 
     const record_0_sql = records[0].generate_insert_sql_postgres();
     let simplified_sql = simplify_sql(record_0_sql);
-    expect(simplified_sql.substring(0, 71)).toBe(' INSERT INTO myschema.person (firstname, person_id) VALUES ($$Steve$$, ');
+    expect(simplified_sql).toBe('INSERT INTO myschema.person (firstname, person_id) VALUES ($$Steve$$, $$some-guid$$) RETURNING *;');
 
     records[0].set_resolved_value('pk', '1234');
 
     const record_1_sql = records[1].generate_insert_sql_postgres();
-    simplified_sql = simplify_sql(record_1_sql.replace(/........-....-....-....-............/, 'some-guid'));
-    expect(simplified_sql).toBe(' INSERT INTO myschema.order (person_id, order_person_pk) VALUES ($$some-guid$$, $$1234$$) RETURNING *;');
+    simplified_sql = simplify_sql(record_1_sql);
+    expect(simplified_sql).toBe('INSERT INTO myschema.order (person_id, order_person_pk) VALUES ($$some-guid$$, $$1234$$) RETURNING *;');
 });
 
 function simplify_sql(sql) {
-    return sql.replace(/[\n\r \t\s]+/g, ' ');
+    return sql.replace(/[\n\r \t\s]+/g, ' ').replace(/........-....-....-....-............/g, 'some-guid').trim();
 }
